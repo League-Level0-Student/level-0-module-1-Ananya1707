f2e328257aebc609198b9418775c87dd8994f3f8
root <root@79ba716a4b2e>
2020-05-20 01:32:38 +0000

    Finished Robot in Space

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,11 +1,29 @@
-
+PImage pepperoni; 
+PImage olive; 
 void setup() {
     size(600,600);
     fill(203,188,129);
     ellipse(300,300,400,400);
     fill(193,43,45);
     ellipse(300,300,350,350);
+    fill(227,214,101);
+    ellipse(300,300,325,325);
+    pepperoni = loadImage("pepperoni.png");
+    olive = loadImage("olives.png");
+    pepperoni.resize(100,100);
+    olive.resize(100,100);
 }
 void draw() {
-    
+  if(mousePressed){
+  }
 }
+   
+void mouseClicked() {
+  if(((int)random(0,2))== 0){
+    print("pepperoni");
+    image(pepperoni,mouseX,mouseY);}
+    else{
+     print("olive");
+    image(olive,mouseX,mouseY);}
+}
+    
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,16 +12,45 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		int score = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String riddle1 = JOptionPane.showInputDialog("What is greater than god, more evil than the devil, the poor have it, the rich need it, and if you eat it, you'll die?");
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if(riddle1.equals("nothing")) {
+			JOptionPane.showMessageDialog(null, "correct!");
+			score = score + 1;
+		}
 
 		// 5. Otherwise, say "wrong" and tell them the answer
+		else {
+			JOptionPane.showMessageDialog(null, "wrong");
+		}
 
 		// 6. Add some more riddles
+		String riddle2 = JOptionPane.showInputDialog("What tastes better than it smells?");
+		if(riddle2.equals("A tongue")) {
+			JOptionPane.showMessageDialog(null, "correct!");
+			score = score + 1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "wrong");
+		}
+		
+		String riddle3 = JOptionPane.showInputDialog("I am an odd number. Take away a letter I become even. What number am I?");
+		if(riddle3.equals("seven")) {
+			JOptionPane.showMessageDialog(null, "correct!");
+			score = score + 1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "wrong");
+		}
+		
+		
 
 		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, "Score: " + score);
 		
 	}
 }
